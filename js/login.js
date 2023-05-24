f5c9ecf8cb9b3a3e233fed5b040bd1655e9c3604
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-23 22:09:13 -0300

    feat: added user card

--- js/login.js
+++ js/login.js
@@ -1,27 +1,25 @@
-
-
 //LISTA DE USUÁRIOS
 let listaDeUsuarios = [
-
     {
+        avatar: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
         nomeCompleto : "José das Couves",
         nomeUsuario : "jose",
-        senhaUsuario : "123456"
-    }
-    ,
+        senhaUsuario : "123456",
+    },
     {
+        avatar: "https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
         nomeCompleto : "João Paulino",
         nomeUsuario : "joao",
         senhaUsuario : "123456"
-    }
-    ,
+    },
     {
+        avatar: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXN1YXJpb3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
         nomeCompleto : "Maria Tomaite",
         nomeUsuario : "maria",
         senhaUsuario : "123456"
-    }
-    ,
+    },
     {
+        avatar: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
         nomeCompleto : "Paulo da Selva",
         nomeUsuario : "paulo",
         senhaUsuario : "123456"
